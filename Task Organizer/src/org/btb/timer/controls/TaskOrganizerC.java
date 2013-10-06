1696b7d1ab12777c703e9c98ceb2b3adf3d4350a
gadulin <gadulin@d7300da2-58ec-11de-8888-c79ed7c8b0c3>
2013-10-06 09:21:52 +0000

    Using a new controller for the app.

---
+++ Task Organizer/src/org/btb/timer/controls/TaskOrganizerC.java
@@ -0,0 +1,67 @@
+package org.btb.timer.controls;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.apache.log4j.Logger;
+import org.btb.timer.gui.TaskOrganizerV;
+import org.btb.timer.gui.TaskPanelV;
+import org.btb.timer.util.GUIThread;
+
+/**
+ * Controller of the Task Organizer
+ * @author Stanislav Milev
+ * @created on Oct 3, 2013
+ */
+public class TaskOrganizerC implements ActionListener {
+
+	static Logger logger = Logger.getLogger(TaskOrganizerC.class);
+
+	private TaskOrganizerV view;
+
+	/**
+	 * Constructor.
+	 */
+	public TaskOrganizerC() {
+		GUIThread gtView = new GUIThread(1);
+		gtView.start();
+		view = gtView.getGui();
+		
+		setListeners(gtView);
+	}
+	
+	/**
+	 * Initializes listeners.
+	 * @param gtView
+	 */
+	private void setListeners(GUIThread gtView) {
+		while(gtView.isNotReady()) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		view.getBtnNewTask().addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object actionEventSource = e.getSource();
+		if (actionEventSource.equals(view.getBtnNewTask())) {
+			addNewTask();
+		}
+	}
+
+	/**
+	 * Adds a new Task panel and sets the listeners to its components.
+	 */
+	private void addNewTask() {
+		logger.info("Adding a new task.");
+		TaskPanelV newTask = new TaskPanelV();
+		view.addTask(newTask);
+		
+	}
+
+}
