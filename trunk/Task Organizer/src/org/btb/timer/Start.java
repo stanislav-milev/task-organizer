2d772d47a6a2ca78067e86de01c7fed8249983d4
gadulin <gadulin@d7300da2-58ec-11de-8888-c79ed7c8b0c3>
2013-10-06 09:21:52 +0000

    Using a new controller for the app.

--- trunk/Task Organizer/src/org/btb/timer/Start.java
+++ trunk/Task Organizer/src/org/btb/timer/Start.java
@@ -2,32 +2,36 @@ package org.btb.timer;
 
 import java.io.IOException;
 
+import org.btb.timer.controls.TaskOrganizerC;
 import org.btb.timer.controls.TimerC;
-import org.btb.timer.core.TaskO;
+import org.btb.timer.data.TaskO;
 import org.btb.timer.util.DataStore;
 import org.btb.timer.util.IConstants;
 
 /**
- * Starting class for Timer_D
+ * Starting class for the Task Organizer
  * @author Stanislav Milev
  * @date 02.09.2008
  */
 public class Start {
 
+	//TODO cleanup
+	
 	/**
 	 * Main method.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		TaskO timerO = null;
-		try {
-			timerO = (TaskO) DataStore.getObject(IConstants.DEFAULT_FILE_PATH);
-		} catch (IOException e) {}
-		
-		if (timerO != null) {
-			new TimerC(timerO);
-		} else
-		new TimerC();
+		new TaskOrganizerC();
+//		TaskO timerO = null;
+//		try {
+//			timerO = (TaskO) DataStore.getObject(IConstants.DEFAULT_FILE_PATH);
+//		} catch (IOException e) {}
+//		
+//		if (timerO != null) {
+//			new TimerC(timerO);
+//		} else
+//		new TimerC();
 	}
 
 }
