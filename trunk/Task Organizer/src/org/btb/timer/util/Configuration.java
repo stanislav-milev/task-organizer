217e6d51e23593d91f80178177a765cedfba91f3
gadulin <gadulin@d7300da2-58ec-11de-8888-c79ed7c8b0c3>
2013-10-06 13:23:14 +0000

    Added maxDisplayedTasks.

--- trunk/Task Organizer/src/org/btb/timer/util/Configuration.java
+++ trunk/Task Organizer/src/org/btb/timer/util/Configuration.java
@@ -3,7 +3,6 @@ package org.btb.timer.util;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -18,21 +17,21 @@ public class Configuration {
 	static Logger log = Logger.getLogger(Configuration.class);
 
     private static Configuration instance;
-    private Map config;
+    
+	private Properties config;
 
     /**
      * Creates the instance of the configuration class.
      */
 	private Configuration(){
-		Properties prop = new Properties();
+		config = new Properties();
 		try {
-		    prop.load(new FileInputStream(IConstants.CONFIGURATION));
+			config.load(new FileInputStream(IConstants.CONFIGURATION));
 	    } catch (FileNotFoundException e) {
 	    	log.error(e);
 	    } catch (IOException e) {
 	    	log.fatal(e);
 	    }
-	    config = prop;
 	}
 
     /**
@@ -44,4 +43,24 @@ public class Configuration {
         }
         return instance;
     }
+    
+    /**
+     * @return the number of max displayed tasks at once
+     */
+    public int getMaxDisplayedTasks() {
+    	if (config.containsKey(IConstants.MAX_DISPLAYED_TASKS)) {
+    		String value = config.get(IConstants.MAX_DISPLAYED_TASKS).toString();
+    		try {
+    			return Integer.valueOf(value);
+    		} catch (NumberFormatException e) {
+    			log.error(value + " is not a valid value for " +
+    					IConstants.MAX_DISPLAYED_TASKS + " a value of " +
+    					IConstants.DEFAULT_MAX_DISPLAYED_TASKS + 
+    					" will be used instead.");
+    		}
+    	}
+    	config.put(IConstants.MAX_DISPLAYED_TASKS, IConstants.DEFAULT_MAX_DISPLAYED_TASKS);
+    	return IConstants.DEFAULT_MAX_DISPLAYED_TASKS;
+    }
+    
 }
